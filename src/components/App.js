7f934458176a43f3a66f45ee5c131d0461a2b727
Skyer3108 <136808389+Skyer3108@users.noreply.github.com>
2024-07-19 20:00:16 +0530

    submitting code-Fri Jul 19 2024 20:00:15 GMT+0530 (India Standard Time)

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,30 @@
 
-import React from "react";
+import React, { useEffect } from "react";
+import { useState } from "react";
 import './../styles/App.css';
 
 const App = () => {
+
+  const [val,setVal]=useState(0)
+
+  const [sum,setSum]=useState()
+
+  useEffect(()=>{
+
+    setSum(sum+val)
+
+  },[val])
   return (
     <div>
         {/* Do not remove the main div */}
+
+        <h1>Sum Calculator</h1>
+
+        <input onChange={(e)=>setVal(e.target.value)} type='number' value={val} />
+
+        <p>Sum :{sum}</p>
+
+
     </div>
   )
 }
